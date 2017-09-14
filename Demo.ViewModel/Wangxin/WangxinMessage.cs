f38f4dfb67a55929fe07d354ef02cc4e38058809
fengxinyi151 <31949659+fengxinyi151@users.noreply.github.com>
2017-09-14 16:03:51 +0800

    旺信demo

---
+++ Demo.ViewModel/Wangxin/WangxinMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BigEndianExtension;
+
+namespace Demo.Library.Wangxin
+{
+    public class WangxinMessage
+    {
+        public string mAccount { get; set; }
+
+        public string mUserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string PhoneNum { get; set; }
+    }
+}
