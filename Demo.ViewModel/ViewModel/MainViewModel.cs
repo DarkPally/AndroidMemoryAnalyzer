2f5b200c3655f78f6b93d5971789b10ddc91c338
DarkPally <593517899@qq.com>
2017-09-14 16:01:57 +0800

    添加一批V与VM

--- Demo.ViewModel/ViewModel/MainViewModel.cs
+++ Demo.ViewModel/ViewModel/MainViewModel.cs
@@ -16,6 +16,17 @@ namespace Demo.Library.ViewModel
         public Browser360ViewModel Browser360ViewModel { get; set; }
         public MicroMsgTradeViewModel MicroMsgTradeViewModel { get; set; }
 
+        public DolphinViewModel DolphinViewModel { get; set; }
+        public BrowserSogouViewModel BrowserSogouViewModel { get; set; }
+        public FetionViewModel FetionViewModel { get; set; }
+        public LaiWangViewModel LaiWangViewModel { get; set; }
+        public MomoViewModel MomoViewModel { get; set; }
+        public OuPengViewModel OuPengViewModel { get; set; }
+        public OutLookViewModel OutLookViewModel { get; set; }
+        public WhatsAppViewModel WhatsAppViewModel { get; set; }
+        public YixinViewModel YixinViewModel { get; set; }
+        public YYTalkViewModel YYTalkViewModel { get; set; }
+
         private static readonly MainViewModel instance = new MainViewModel();
         public static MainViewModel GetInstance() { return instance; }
         private MainViewModel()
@@ -27,6 +38,17 @@ namespace Demo.Library.ViewModel
             ECloudViewModel = new ECloudViewModel();
             Browser360ViewModel = new Browser360ViewModel();
             MicroMsgTradeViewModel = new MicroMsgTradeViewModel();
+
+            DolphinViewModel = new DolphinViewModel();
+            BrowserSogouViewModel = new BrowserSogouViewModel();
+            FetionViewModel = new FetionViewModel();
+            LaiWangViewModel = new LaiWangViewModel();
+            MomoViewModel = new MomoViewModel();
+            OuPengViewModel = new OuPengViewModel();
+            OutLookViewModel = new OutLookViewModel();
+            WhatsAppViewModel = new WhatsAppViewModel();
+            YixinViewModel = new YixinViewModel();
+            YYTalkViewModel = new YYTalkViewModel();
         }
 
         public static void InitializeWithParams(string type,string path)
@@ -66,6 +88,56 @@ namespace Demo.Library.ViewModel
                 case "com.tencent.mmtools":
                     instance.MicroMsgTradeViewModel.FilePath = path;
                     instance.MicroMsgTradeViewModel.ExcuteAnalyzeFile();
+                    instance.startType = type;
+                    break;
+                case "com.dolphin.browser.xf":
+                    instance.DolphinViewModel.FilePath = path;
+                    instance.DolphinViewModel.ExcuteAnalyzeFile();
+                    instance.startType = type;
+                    break;
+                case "sogou.mobile.explorer":
+                    instance.BrowserSogouViewModel.FilePath = path;
+                    instance.BrowserSogouViewModel.ExcuteAnalyzeFile();
+                    instance.startType = type;
+                    break;
+                case "cn.com.fetion":
+                    instance.FetionViewModel.FilePath = path;
+                    instance.FetionViewModel.ExcuteAnalyzeFile();
+                    instance.startType = type;
+                    break;
+                case "com.alibaba.android.babylon":
+                    instance.LaiWangViewModel.FilePath = path;
+                    instance.LaiWangViewModel.ExcuteAnalyzeFile();
+                    instance.startType = type;
+                    break;
+                case "com.immomo.momo":
+                    instance.MomoViewModel.FilePath = path;
+                    instance.MomoViewModel.ExcuteAnalyzeFile();
+                    instance.startType = type;
+                    break;
+                case "com.oupeng.mini.android":
+                    instance.OuPengViewModel.FilePath = path;
+                    instance.OuPengViewModel.ExcuteAnalyzeFile();
+                    instance.startType = type;
+                    break;
+                case "com.microsoft.office.outlook":
+                    instance.OutLookViewModel.FilePath = path;
+                    instance.OutLookViewModel.ExcuteAnalyzeFile();
+                    instance.startType = type;
+                    break;
+                case "com.whatsapp":
+                    instance.WhatsAppViewModel.FilePath = path;
+                    instance.WhatsAppViewModel.ExcuteAnalyzeFile();
+                    instance.startType = type;
+                    break;
+                case "im.yixin":
+                    instance.YixinViewModel.FilePath = path;
+                    instance.YixinViewModel.ExcuteAnalyzeFile();
+                    instance.startType = type;
+                    break;
+                case "com.duowan.mobile":
+                    instance.YYTalkViewModel.FilePath = path;
+                    instance.YYTalkViewModel.ExcuteAnalyzeFile();
                     instance.startType = type;
                     break; 
                 default:
